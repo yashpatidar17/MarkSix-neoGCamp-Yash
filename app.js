1beedc9152a350c0cd4b8bf122224786ba525f6b
Yash Patidar <yashpatidar964@gmail.com>
2023-01-07 02:11:10 +0530

    Final Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var translateButton = document.querySelector("#translateBtn");
+var textAreaInput = document.querySelector("#text-area");
+var textAreaoutput = document.querySelector("#output-area");
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationUrl(text){
+    return serverUrl + "?" + "text=" +text;
+}
+
+function eventAfterClick(event){
+    var textInput = textAreaInput.value;
+
+    fetch(getTranslationUrl(textInput))
+        .then(response => response.json())
+        .then(json => {
+            textAreaoutput.innerText = json.contents.translated;
+             
+        })
+}
+
+translateButton.addEventListener("click",eventAfterClick);
